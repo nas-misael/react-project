e2289a1b130d24b758f82d3ef2021967271ea087
Misael Nascimento <nascimentomisael3@gmail.com>
2021-09-27 10:20:17 -0300

    Componentizacao o sistema

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import {ListaDeNotas} from './components/listaNotas.jsx'
+import {FormularioCadastro} from './components/formularioCadastro.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <section>
+    <FormularioCadastro />
+    <ListaDeNotas />
+  </section>
   );
 }
 
